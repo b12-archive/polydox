18f962d70abbc545ec145b1c0ad4d5d77020a2aa
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-24 16:41:38 +0200

    Yippeee! It works!

--- module/bin/polydox.js
+++ module/bin/polydox.js
@@ -2,11 +2,15 @@
 
 const {stdout, stderr, exit, argv} = process;
 
+const forEach = require('1-liners/forEach');
+
 const args = argv.slice(2);
 const flags = {};
-args.forEach((argument) => {
-  if (argument.charAt(0) === '-') flags[argument] = true;
-});
+const files = [];
+forEach((arg) => {
+  if (arg.charAt(0) === '-') flags[arg] = true;
+  else files.push(arg);
+}, args);
 
 if (!args.length) {
   stderr.write(require('./help/usage'));
@@ -22,3 +26,36 @@ if (flags['--help']) stdout.write([
 ].join('\n\n'));
 
 if (flags['-h'] || flags['--help']) exit(0);
+
+const {readFileSync} = require('fs');
+const {resolve} = require('path');
+
+const curry = require('1-liners/curry');
+const replace = require('1-liners/replace');
+const extend = require('1-liners/extend');
+const {parseComments} = require('dox');
+
+const indent = curry(replace)(/^(?!$)/mg, '  ');
+const toJson = (object) => JSON.stringify(object, null, 2);
+
+stdout.write('[');
+
+forEach((sourceFile, fileId) => {
+  const decorate = curry(extend)({sourceFile});
+
+  const source =
+    readFileSync(resolve(process.cwd(), sourceFile), {encoding: 'utf8'})
+  ;
+
+  forEach(
+    (comment, commentId) => stdout.write(
+      ((fileId === 0 && commentId === 0) ? '\n' : ',\n') +
+      indent(toJson(decorate(comment)))
+    ),
+    parseComments(source)
+  );
+}, files);
+
+stdout.write('\n]\n');
+
+exit(0);
