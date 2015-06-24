ec3b77d460694e96dcf16831084ddc84e4a1f518
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-24 13:40:48 +0200

    Rework argument parsing

--- module/bin/polydox.js
+++ module/bin/polydox.js
@@ -1,20 +1,24 @@
 #! /usr/bin/env node
 
-const {stdout, exit, argv} = process;
+const {stdout, stderr, exit, argv} = process;
 
-const contains = (element, array) => array.indexOf(element) !== -1;
+const args = argv.slice(2);
+const flags = {};
+args.forEach((argument) => {
+  if (argument.charAt(0) === '-') flags[argument] = true;
+});
 
-if (contains('-h', argv)) {
-  stdout.write(require('./help/synopsis'));
-  exit(0);
+if (!args.length) {
+  stderr.write(require('./help/synopsis'));
+  exit(1);
 }
 
-if (contains('--help', argv)) {
-  stdout.write([
-    require('./help/usage'),
-    require('./help/options'),
-    require('./help/examples'),
-  ].join('\n\n'));
+if (flags['-h']) stdout.write(require('./help/synopsis'));
 
-  exit(0);
-}
+if (flags['--help']) stdout.write([
+  require('./help/usage'),
+  require('./help/options'),
+  require('./help/examples'),
+].join('\n\n'));
+
+if (flags['-h'] || flags['--help']) exit(0);
