01e1350eb151a1f134f81b36b9d99270e58b5eea
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-24 12:30:23 +0200

    Restructure test files

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-const test = require('tape-catch');
-
-test('Programmatic usage:  Fails', (is) => {
-  is.throws(
-    () => require('./module/index'),
-    /the original `dox` is perfectly ok/i,
-    'with a helpful message'
-  );
-
-  is.end();
-});
+import './test/index';
