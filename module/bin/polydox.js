49bcee7c4e265f9fbde75e823df95377633bab7a
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-24 13:32:49 +0200

    We don’t need yargs after all

--- module/bin/polydox.js
+++ module/bin/polydox.js
@@ -1,14 +1,20 @@
 #! /usr/bin/env node
 
-const {argv} = require('yargs');
-const {stdout, exit} = process;
+const {stdout, exit, argv} = process;
 
-if (argv.h) stdout.write(require('./help/synopsis'));
+const contains = (element, array) => array.indexOf(element) !== -1;
 
-if (argv.help) stdout.write([
-  require('./help/usage'),
-  require('./help/options'),
-  require('./help/examples'),
-].join('\n\n'));
+if (contains('-h', argv)) {
+  stdout.write(require('./help/synopsis'));
+  exit(0);
+}
 
-if (argv.h || argv.help) exit(0);
+if (contains('--help', argv)) {
+  stdout.write([
+    require('./help/usage'),
+    require('./help/options'),
+    require('./help/examples'),
+  ].join('\n\n'));
+
+  exit(0);
+}
